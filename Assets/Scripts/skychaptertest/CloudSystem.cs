1dc7e4faa14a0d318e57722888bc41fc1fde4a36
gmltjr1 <102124460+gmltjr1@users.noreply.github.com>
2025-07-06 18:46:48 +0900

    작업 사라진거 복구
    Revert "Merge branch 'main' of https://github.com/Doumoman/TLTower"에서 작업이 사라졌길래 취소함

--- Assets/Scripts/skychaptertest/CloudSystem.cs
+++ Assets/Scripts/skychaptertest/CloudSystem.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class CloudSystem : MonoBehaviour
 {
     public GameObject checkPoint;
-    private List<JointMaker> nodes = new List<JointMaker>();
+    [SerializeField]private List<JointMaker> nodes = new List<JointMaker>();
 
     public static CloudSystem Instance;
 
@@ -19,6 +19,7 @@ public class CloudSystem : MonoBehaviour
     public void SetCheckPoint(GameObject go)
     {
         checkPoint = go;
+        go.AddComponent<JointMaker>();
         nodes.Clear();
     }
 
