7b060768b478872aaf2f90c7fc32b1e88b6b3b2f
Doumoman <mp3247472@naver.com>
2025-07-08 01:01:29 +0900

    메인화면 완성

---
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader: MonoBehaviour
+{
+    public void LoadMain()
+    {
+        SceneManager.LoadScene("main");
+        Time.timeScale = 1.0f;
+    }
+    public void LoadGame()
+    {
+         SceneManager.LoadScene("TLTower");
+         Time.timeScale = 1.0f;
+    }
+}
