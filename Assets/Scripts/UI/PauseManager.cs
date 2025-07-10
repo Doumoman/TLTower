9f1319fea4283c50c6c17a4693735e1dab243371
Doumoman <mp3247472@naver.com>
2025-07-10 18:53:35 +0900

    수정

--- Assets/Scripts/UI/PauseManager.cs
+++ Assets/Scripts/UI/PauseManager.cs
@@ -1,10 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
-
 
 public class PauseManager : MonoBehaviour
 {
@@ -20,14 +15,19 @@ public class PauseManager : MonoBehaviour
 
         // 처음엔 UI만 꺼 두고, 이 스크립트 오브젝트는 켜 둔다
         pausePanelRoot.SetActive(false);
+
+        // ‘뒤로가기’로 바로 앱이 종료되지 않도록
         Input.backButtonLeavesApp = false;
     }
 
+#if UNITY_ANDROID || UNITY_EDITOR   // ▶ 에디터·안드로이드에서만 동작
     void Update()
     {
+        // 안드로이드 ‘뒤로가기’ 버튼 == KeyCode.Escape
         if (Input.GetKeyDown(KeyCode.Escape))
             TogglePause();
     }
+#endif
 
     /* ---------- 외부에서도 호출 가능 ---------- */
     public void TogglePause()
