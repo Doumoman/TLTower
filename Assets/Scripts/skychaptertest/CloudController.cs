c8324c587ad7b2eeda7c31e94557526a88e4d5d2
gmltjr1 <102124460+gmltjr1@users.noreply.github.com>
2025-08-04 00:31:15 +0900

    불필요한 using문 제거

--- Assets/Scripts/skychaptertest/CloudController.cs
+++ Assets/Scripts/skychaptertest/CloudController.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Linq;
-using System.Reflection;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.WSA;
 
 public enum CloudState { flow, Dragging, Dropped };
 [RequireComponent(typeof(Rigidbody2D))]
